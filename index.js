df5b880e2030f8b73fea16a1dd6f002da466047b
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2023-11-21 19:27:48 -0700

    strings practice

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = 'defined';
+let welcomeMessage = "Welcome to Flatbook, defined!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = 'Welcome, d!';
